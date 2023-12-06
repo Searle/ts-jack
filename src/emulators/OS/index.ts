bc90089d2140741f17b1e6e2a4857f3e1c9fb5b7
Dietrich Raisin <info1@raisin.de>
2023-12-06 21:20:50 +0100

    WIP

---
+++ src/emulators/OS/index.ts
@@ -0,0 +1,13 @@
+import Array from "./Array.jack";
+import Math from "./Math2.jack";
+import Memory from "./Memory.jack";
+import Output from "./Output2.jack";
+import Screen from "./Screen2.jack";
+import String from "./String.jack";
+import Sys from "./Sys.jack";
+import Keyboard from "./Keyboard.jack";
+
+export const os = () => {
+    // return Array + Math + Memory + Output + Screen + String + Sys + Keyboard;
+    return Array + Math + Memory + Output + Screen + String + Sys;
+};
